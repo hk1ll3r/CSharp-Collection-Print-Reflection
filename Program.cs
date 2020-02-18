bf3b545ee86e31df50acdb5a1bf180bfe3925e4e
Hoss Shah <hossein.shbz@gmail.com>
2020-02-18 13:43:34 -0800

    initial

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using NoSuchStudio.Common;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 1 && args[0] == "-v") {
+            ReflectionToStringExts.verbose = true;
+        }
+        // list
+        Console.WriteLine("flat list");
+        var strList = new List<string>() {"one", "two", "three"};
+        Console.WriteLine("ToString: " + strList.ToString());
+        Console.WriteLine("ToStringExt: " + strList.ToStringExt());
+        Console.WriteLine("");
+
+        // list of list
+        Console.WriteLine("List of lists");
+        var listOfLists = new List<List<string>>() { new List<string>() {"one", "two"}, new List<string>() {"three", "four"}};
+        Console.WriteLine("ToString: " + listOfLists.ToString());
+        Console.WriteLine("ToStringExt: " + listOfLists.ToStringExt());
+        Console.WriteLine("");
+
+        // list of list of list
+        Console.WriteLine("List of Lists of Lists");
+        var listOfListsOfLists = new List<List<List<string>>>() {
+            new List<List<string>>() { new List<string>() {"one", "two"}, new List<string>() {"three", "four"}},
+                new List<List<string>>() { new List<string>() {"five", "six"}, new List<string>() {"seven", "eight"}}};
+        Console.WriteLine("ToString: " + listOfListsOfLists);
+        Console.WriteLine("ToStringExt: " + listOfListsOfLists.ToStringExt());
+        Console.WriteLine("");
+
+        // dictionary
+        Console.WriteLine("Dictionary");
+        var flatDic = new Dictionary<int, string>() {
+            [1] = "one",
+            [2] = "two"
+        };
+        Console.WriteLine("ToString: " + flatDic);
+        Console.WriteLine("ToStringExt: " + flatDic.ToStringExt());
+        Console.WriteLine("");
+
+        // dictionary of dictionary
+        Console.WriteLine("Dictionary of Dictionaries");
+        var dic2 = new Dictionary<int, Dictionary<string, string>>() {
+            [1] = new Dictionary<string, string>() {
+                ["one"] = "first",
+                ["two"] = "second"
+            },
+            [2] = new Dictionary<string, string>() {
+                ["three"] = "third",
+                ["four"] = "forth"
+            }
+        };
+        Console.WriteLine("ToString: " + dic2);
+        Console.WriteLine("ToStringExt: " + dic2.ToStringExt());
+        Console.WriteLine("");
+
+        // dictionary of list
+        Console.WriteLine("Dictionary of Lists");
+        var dicOfList = new Dictionary<int, List<string>>() {
+            [1] = new List<string>() {"one", "two"},
+            [2] = new List<string>() {"three", "four"},
+        };
+        Console.WriteLine("ToString: " + dicOfList);
+        Console.WriteLine("ToStringExt: " + dicOfList.ToStringExt());
+        Console.WriteLine("");
+
+        // list of dictionary
+        Console.WriteLine("List of Dictionaries");
+        var listOfDic = new List<Dictionary<int, string>>() {
+            new Dictionary<int, string>() {
+                [1] = "one",
+                [2] = "two"
+            },
+            new Dictionary<int, string>() {
+                [3] = "three",
+                [4] = "four"
+            }
+        };
+        Console.WriteLine("ToString: " + listOfDic);
+        Console.WriteLine("ToStringExt: " + listOfDic.ToStringExt());
+        Console.WriteLine("");
+
+        // HashSet
+        Console.WriteLine("HashSet");
+        var hashSet = new HashSet<string>() {"one", "two", "three"};
+        Console.WriteLine("ToString: " + hashSet.ToString());
+        Console.WriteLine("ToStringExt: " + hashSet.ToStringExt());
+        Console.WriteLine("");
+    }
+}
---
+++ ToStringExtsReflection.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace NoSuchStudio.Common
+{
+    public static class ReflectionToStringExts
+    {
+        public static bool verbose = false;
+        public static string ToStringExtCollection<T>(this ICollection<T> obj, int d = 0)
+        {
+            return (verbose ? "Collection<" + typeof(T).Name + ">" : "") + "{" + string.Join(", ", obj.Select(o => o.ToStringExt(d + 1))) + "}";
+        }
+        public static string ToStringExtKeyValuePair<U, V>(this KeyValuePair<U, V> kvp, int d = 0)
+        {
+            return (verbose ? "KeyValuePair<" + typeof(U).Name + "," + typeof(V).Name  + ">" : "") +
+                kvp.Key.ToStringExt(d + 1) +
+                " => " +
+                kvp.Value.ToStringExt(d + 1);
+        }
+        public static string ToStringExt<T>(this T obj, int d = 0)
+        {
+            string res = null;
+            Type TType = typeof(T);
+            if (TType.IsGenericType
+                && (TType.GetGenericTypeDefinition() == typeof(KeyValuePair<,>)))
+            {
+                Type kvpType = typeof(KeyValuePair<,>).MakeGenericType(TType.GetGenericArguments()[0], TType.GetGenericArguments()[1]);
+                res = ReflectionToStringExts.ToStringExtKeyValuePair(obj.CastToReflected(kvpType), d + 1);
+            }
+            else
+            {
+                foreach (var IType in TType.GetInterfaces()) 
+                {
+                    if (IType.IsGenericType
+                        && (IType.GetGenericTypeDefinition() == typeof(ICollection<>)))
+                    {
+                        res = ReflectionToStringExts.ToStringExtCollection(obj.CastToReflected(IType), d + 1);
+                        break;
+                    }
+                }
+            }
+            if (res == null)
+            {
+                res = obj.ToString();
+            }
+            return res;
+        }
+
+        public static T CastTo<T>(this object o) => (T)o;
+
+        public static dynamic CastToReflected(this object o, Type type)
+        {
+            var methodInfo = typeof(ReflectionToStringExts).GetMethod(nameof(CastTo), BindingFlags.Static | BindingFlags.Public);
+            var genericArguments = new[] { type };
+            var genericMethodInfo = methodInfo?.MakeGenericMethod(genericArguments);
+            return genericMethodInfo?.Invoke(null, new[] { o });
+        }
+    }
+}
